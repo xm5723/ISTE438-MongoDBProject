38bdea57428e5cf7b12fa7bd5b2b7a96aedfff5f
Tai Hartman <hartman.tai@gmail.com>
2022-03-16 20:08:59 -0400

    Got stuff working

--- addData.js
+++ addData.js
@@ -2,21 +2,36 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://admin:Group5@cluster0.0vxli.mongodb.net/CafeData?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    // const collection = client.db("CafeData").collection("CafeInfo");
-    // perform actions on the collection object
-    console.log('here');
-    findOneCafe();
-    // client.close();
-});
+// client.connect(err => {
+//     // const collection = client.db("CafeData").collection("CafeInfo");
+//     // perform actions on the collection object
+//     console.log('here');
+//     findOneCafe();
+//     // client.close();
+// });
 
-
-var findOneCafe = function () {
-    return new Promise(function (resolve, reject) {
-        const collection = client.db("CafeData").collection("CafeInfo");
-
-        var cursor = collection.findOne({ 'Company Name': 'Hard Rock Cafe' });
-        console.log(cursor);
-        client.close();
-    })
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('CafeData');
+        const cafes = database.collection('CafeInfo');
+        // Query for a movie that has the title 'Back to the Future'
+        const query = { 'Company Name': 'Hard Rock Cafe' };
+        const cafe = await cafes.findOne(query);
+        console.log(cafe);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
 }
+run().catch(console.dir);
+
+// var findOneCafe = function () {
+//     return new Promise(function (resolve, reject) {
+//         const collection = client.db("CafeData").collection("CafeInfo");
+
+//         var cursor = collection.findOne({ 'Company Name': 'Hard Rock Cafe' });
+//         console.log(cursor);
+//         client.close();
+//     })
+// }
