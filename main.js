435e9befcd0f08897adff448113fc39436c7ae41
Xing Meng <xm5723@g.rit.edu>
2022-03-17 17:43:57 -0400

    New Changes

--- main.js
+++ main.js
@@ -10,20 +10,13 @@ async function findCafe(search) {
         const database = client.db('CafeData');
         const cafes = database.collection('CafeInfo');
 
-        //const query = { 'Company Name': /\b\b/i };
         const query = { 'Company Name': new RegExp(`\\b${search}`, 'gi')};
-        //const query = {text: /\b${search}\b/i}
-        //const query = {'Company Name': new RegExp(`/\b${search}\b/i`)};
-        //const query = {text: /\b${search}\b/i}
         const options = {
             sort:  {Address: 1},
             projection: {_id: 0, 'Company Name':1, Address: 1, Phone: 1, Link: 1}
         }
 
-        //const query = { 'Company Name': { $regex: `/\b${search}\b/i` }};
         const cursor = cafes.find(query, options);
-
-        //cafes.find( { 'Company Name': { $regex: `/\b${search}\b/i` } } )
 
         // print a message if no documents were found
         if ((await cursor.count()) === 0) {
@@ -32,20 +25,6 @@ async function findCafe(search) {
 
         // replace console.dir with your callback to access individual elements
         await cursor.forEach(console.dir);
-
-        // const cafe = cafes.find(query);
-        // while (cafe.hasNext()){
-        //     //printjson(cursor.next());
-        //     doc = cafe.next();
-        //     console.log(doc.Address);
-        //     console.log(doc.Rating);
-        //     console.log(doc.text);
-        // }
-        // console.log(cafe);
-        
-        // var query = new RegExp(`/\b${search}\b/i`);
-        // const cafe = await cafes.findOne(query);
-        // console.log(cafe);
     } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
