6eeb7ee9708b67c142d1b9a1e6fa0e6a264c5687
Xing Meng <xm5723@g.rit.edu>
2022-03-28 00:02:55 -0400

    Find Image Update

--- main.js
+++ main.js
@@ -158,35 +158,35 @@ async function findImage(objectId){
             }
         }
         else{
-        const query = { 'cafeID': new RegExp(`\\b12345678`, 'gi')};
-    
-        const options = {
-            projection: {_id:{"$toString": "$_id"}, filename: 1}
+            const query = { 'cafeID': new RegExp(`\\b12345678`, 'gi')};
+        
+            const options = {
+                projection: {_id:{"$toString": "$_id"}, filename: 1}
+            }
+            const cursor = cafesImages.find(query, options);
+            // print a message if no documents were found
+            if ((await cursor.count()) === 0) {
+                console.log("No documents found!");
+            }
+            await cursor.forEach(function(item){
+                resultDefaultImage.push(item);
+            }); 
+
+            data = resultDefaultImage[0].filename;
+            console.log("data", data);
+
+            const bucket = new GridFSBucket(database, {bucketName: 'fs'});
+
+            bucket.openDownloadStreamByName(data).
+            pipe(fs.createWriteStream('/Users/xingmeng/Documents/school/2022Spring/ISTE483/project/image/outputFile.png')).
+            on('error', function(error) {
+                assert.ifError(error);
+            }).
+            on('finish', function() {
+                process.exit(0);
+            });
+            return data;
         }
-        const cursor = cafesImages.find(query, options);
-        // print a message if no documents were found
-        if ((await cursor.count()) === 0) {
-            console.log("No documents found!");
-        }
-        await cursor.forEach(function(item){
-            resultDefaultImage.push(item);
-        }); 
-
-        data = resultDefaultImage[0].filename;
-        console.log("data", data);
-
-        const bucket = new GridFSBucket(database, {bucketName: 'fs'});
-
-        bucket.openDownloadStreamByName(data).
-        pipe(fs.createWriteStream('/Users/xingmeng/Documents/school/2022Spring/ISTE483/project/image/outputFile.png')).
-        on('error', function(error) {
-            assert.ifError(error);
-        }).
-        on('finish', function() {
-            process.exit(0);
-        });
-        return data;
-    }
         
     }
     finally {
@@ -324,45 +324,34 @@ async function findLocations(longitude, latitude) {
 }
 
 //for frontend: update UI inside the 'then' 
-search('Hard Rock cafe').then(function(results){
-    //update UI here using results array
-    console.log(results);
-}).catch(console.dir);
+// search('Hard Rock cafe').then(function(results){
+//     //update UI here using results array
+//     console.log(results);
+// }).catch(console.dir);
 
 
-//for frontend: update UI inside the 'then' 
-findCafeDetails('Hard Rock cafe').then(function(results){
-    //update UI here using results array
-    for (var i=0; i<results.length;i++){
-        console.log(results[i].Address);
-    }
-    console.log(results);
-}).catch(console.dir);
+// //for frontend: update UI inside the 'then' 
+// findCafeDetails('Hard Rock cafe').then(function(results){
+//     //update UI here using results array
+//     for (var i=0; i<results.length;i++){
+//         console.log(results[i].Address);
+//     }
+//     console.log(results);
+// }).catch(console.dir);
 
 
-// setCafeComment('6211595e83f42d30651d2e5e','this is a test comment');
+// // setCafeComment('6211595e83f42d30651d2e5e','this is a test comment');
 
-getCommentsByID('6211595e83f42d30651d2e5e').then(function(results){
-    //update UI here using results array
-    for (var i=0; i<results.length;i++){
-        console.log(results[i].comment);
-    }
+// getCommentsByID('6211595e83f42d30651d2e5e').then(function(results){
+//     //update UI here using results array
+//     for (var i=0; i<results.length;i++){
+//         console.log(results[i].comment);
+//     }
 
-}).catch(console.dir);
+// }).catch(console.dir);
 
-//6211595e83f42d30651d2e3e
-findImage('6211595e83f42d30651d2e50').then(function(data){
-    //update UI here using results array
-   
-    // var imageUrl = new URL(data.toString);
-    console.log(data);
-    // var readStream = fs.createReadStream({filename:imageUrl});
-    // readStream.pipe(res);
-}).catch(console.dir);
-
-// createLocation();
-
-// findLocations(12.97406376, 77.59459773).then(function(data){
+// //6211595e83f42d30651d2e3e
+// findImage('6211595e83f42d30651d2e50').then(function(data){
 //     //update UI here using results array
    
 //     // var imageUrl = new URL(data.toString);
@@ -370,3 +359,14 @@ findImage('6211595e83f42d30651d2e50').then(function(data){
 //     // var readStream = fs.createReadStream({filename:imageUrl});
 //     // readStream.pipe(res);
 // }).catch(console.dir);
+
+createLocation();
+
+findLocations(12.97406376, 77.59459773).then(function(data){
+    //update UI here using results array
+   
+    // var imageUrl = new URL(data.toString);
+    console.log(data);
+    // var readStream = fs.createReadStream({filename:imageUrl});
+    // readStream.pipe(res);
+}).catch(console.dir);
