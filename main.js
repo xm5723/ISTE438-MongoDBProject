1e7cb067ad1a57f0c4bdc9ca954f34fccdc02f49
Xing Meng <xm5723@g.rit.edu>
2022-03-27 20:50:08 -0400

    Set Default Image

--- main.js
+++ main.js
@@ -118,6 +118,7 @@ async function getCommentsByID(cafeObjectId) {
 async function findImage(objectId){
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
     var results = [];
+    var resultDefaultImage = [];
     var data = '';
     try{
         await client.connect();
@@ -137,8 +138,43 @@ async function findImage(objectId){
             results.push(item);
         }); 
 
-        data = results[0].filename;
+        console.log("results", results)
+
+        if(results != null && results != '') {
+            data = results[0].filename;
+            console.log("data", data);
+            if (data !== 'undefined'){
+                const bucket = new GridFSBucket(database, {bucketName: 'fs'});
+
+                bucket.openDownloadStreamByName(data).
+                pipe(fs.createWriteStream('/Users/xingmeng/Documents/school/2022Spring/ISTE483/project/image/outputFile.png')).
+                on('error', function(error) {
+                    assert.ifError(error);
+                }).
+                on('finish', function() {
+                    process.exit(0);
+                });
+                return data;
+            }
+        }
+        else{
+        const query = { 'cafeID': new RegExp(`\\b12345678`, 'gi')};
+    
+        const options = {
+            projection: {_id:{"$toString": "$_id"}, filename: 1}
+        }
+        const cursor = cafesImages.find(query, options);
+        // print a message if no documents were found
+        if ((await cursor.count()) === 0) {
+            console.log("No documents found!");
+        }
+        await cursor.forEach(function(item){
+            resultDefaultImage.push(item);
+        }); 
+
+        data = resultDefaultImage[0].filename;
         console.log("data", data);
+
         const bucket = new GridFSBucket(database, {bucketName: 'fs'});
 
         bucket.openDownloadStreamByName(data).
@@ -149,8 +185,9 @@ async function findImage(objectId){
         on('finish', function() {
             process.exit(0);
         });
-
         return data;
+    }
+        
     }
     finally {
         // Ensures that the client will close when you finish/error
@@ -159,9 +196,9 @@ async function findImage(objectId){
     }
 }
 
-async function findLocations(location) {
+async function createLocation(){
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+    results = [];
     try {
         await client.connect();
         const database = client.db('CafeData');
@@ -172,9 +209,9 @@ async function findLocations(location) {
                 // update document, using its own properties
                 // Check to see if latitude is in the document
                 //Sloppy coding alert - I hould have check longitude also, but it works here
-                var ll = {longitude : e.longitude, latitude: e.latitude};
-                var lla =[]; //an array
-                if ('latitude' in e && e.latitude !== ""){
+                if ('Latitude' in e && e.Latitude !== ""){
+                    var ll = {Longitude : e.Longitude, Latitude: e.Latitude};
+                    var lla =[]; //an array
                     //fills the array with longitude and latitude
                     Object.keys(ll).forEach(function(key) {
                     var val = ll[key];
@@ -183,33 +220,105 @@ async function findLocations(location) {
                 var p = "Point";
                 // Create location variable in document - see how this compares with the slides
                 loc = {type: p, coordinates: lla};     	
-                     
+                        
                 // save the updated document
-                wc = db.Tweets.updateOne({_id: e._id}, {$set:{loc}})
+                wc = cafes.updateOne({_id: e._id}, {$set:{loc}})
                 }
             }
-        ) 	
+        )	
     
-        docs = cafes.createIndex( { loc : "2dsphere" });
-        // //SHOW INDEXES
-        // docs = cafes.getIndexes();
-        console.log("doc", doc)
-
-        // const cursor = cafes.find(query, options);
-
-        // // print a message if no documents were found
-        // if ((await cursor.count()) === 0) {
-        //     console.log("No documents found!");
-        // }
-        
-        // await cursor.forEach(function(item){
-        //     results.push(item);
-            
-        // }); 
-        // // console.log(results);
-        // return results;
-    } finally {
+        cafes.createIndex( { loc : "2dsphere" });
+        console.log("Here");
+        return results;
+    }
+    catch(error) {
+        console.log("That did not go well");
+        console.error(error);
+    }
+    finally {
         // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+async function findLocations(longitude, latitude) {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    results = [];
+    try {
+        await client.connect();
+        const database = client.db('CafeData');
+        const cafes = database.collection('CafeInfo');
+
+        // const cursor = cafes.find(
+        //     { loc:
+        //         { $near:
+        //             { $geometry:
+        //                 {
+        //                     type: "Point",
+        //                     coordinates: [longitude, latitude]
+        //                 },
+        //                 $maxDistance: 1000
+        //             }
+        //         }
+        //     }
+        // )
+
+        // const cursor = cafes.find(
+        //     {
+        //       loc:
+        //         { $near:
+        //            {
+        //              $geometry: { type: "Point",  coordinates: [longitude, latitude] },
+        //              $minDistance: 1000,
+        //              $maxDistance: 5000
+        //            }
+        //         }
+        //     }
+        //  )
+
+        const options = {
+            sort:  {Address: 1},
+            projection: {'Company Name':1, Address: 1}
+        }
+
+        const query = {
+            "loc": {
+              $near: {
+                $geometry: { type: "Point", coordinates: [longitude, latitude] },
+                $maxDistance: 1000,
+              },
+            },
+          };
+        const cursor = cafes.find(query, options);
+
+        // print a message if no documents were found
+        if ((await cursor.count()) === 0) {
+            console.log("No documents found!");
+        }
+        
+        await cursor.forEach(function(item){
+            results.push(item); 
+        }); 
+
+        process.on('unhandledRejection', error => {
+            // Will print "unhandledRejection err is not defined"
+            console.log('unhandledRejection', error.message);
+          });
+          
+        new Promise((_, reject) => reject(new Error('woops'))).
+            catch(error => {
+            // Will not execute
+            console.log('caught', err.message);
+            });
+        return results;
+    }
+    catch(error) {
+        console.log("That did not go well");
+        console.error(error);
+    }
+    finally {
+        // Ensures that the client will close when you finish/error
+        console.log("close")
         await client.close();
     }
 }
@@ -241,7 +350,8 @@ getCommentsByID('6211595e83f42d30651d2e5e').then(function(results){
 
 }).catch(console.dir);
 
-findImage('6211595e83f42d30651d2e3e').then(function(data){
+//6211595e83f42d30651d2e3e
+findImage('6211595e83f42d30651d2e50').then(function(data){
     //update UI here using results array
    
     // var imageUrl = new URL(data.toString);
@@ -250,11 +360,13 @@ findImage('6211595e83f42d30651d2e3e').then(function(data){
     // readStream.pipe(res);
 }).catch(console.dir);
 
-findLocations('New York').then(function(data){
-    //update UI here using results array
+// createLocation();
+
+// findLocations(12.97406376, 77.59459773).then(function(data){
+//     //update UI here using results array
    
-    // var imageUrl = new URL(data.toString);
-    console.log(data);
-    // var readStream = fs.createReadStream({filename:imageUrl});
-    // readStream.pipe(res);
-}).catch(console.dir);
+//     // var imageUrl = new URL(data.toString);
+//     console.log(data);
+//     // var readStream = fs.createReadStream({filename:imageUrl});
+//     // readStream.pipe(res);
+// }).catch(console.dir);
